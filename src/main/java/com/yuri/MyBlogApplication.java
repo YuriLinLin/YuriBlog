1d801d29206bd48c9e6cb4abec88335543a6f476
YuriLinLin <610195478@qq.com>
2020-02-03 23:12:37 +0800

    first commit

---
+++ src/main/java/com/yuri/MyBlogApplication.java
@@ -0,0 +1,15 @@
+package com.yuri;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.yuri.dao")
+//@EnableCaching
+public class MyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogApplication.class, args);
+    }
+}
